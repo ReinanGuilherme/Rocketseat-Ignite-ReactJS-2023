898b7b5672122dd7dd871b7a34dabf3fe2144f6d
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.local>
2023-03-07 18:04:59 -0300

    Atualizando Projeto 02 - Modulo 02 - Finalizacao Home

--- projeto02/src/App.tsx
+++ projeto02/src/App.tsx
@@ -1,8 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
